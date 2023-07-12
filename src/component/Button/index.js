7d2e55fa50b563f1cc793924dcbfa8d308d57a44
Hoang  Son <son.lyhoang2014@gmail.com>
2023-07-13 01:29:26 +0700

    Xây dựng UI phần Header #3

--- src/component/Button/index.js
+++ src/component/Button/index.js
@@ -1,15 +1,65 @@
+import { faLess } from "@fortawesome/free-brands-svg-icons";
+import { type } from "@testing-library/user-event/dist/type";
 import classNames from "classnames/bind";
+import { Link } from "react-router-dom";
 import styles from "./Button.module.scss"
 
 const cx = classNames.bind(styles)
 
-function Button ({ to, href,children, onClick }){
+function Button({to,
+    href,
+    className,
+    primary=false,
+    outline=false,
+    text=false,
+    rounded=false,
+    disabled=false,
+    small=false,
+    large=false,
+    children,
+    leftIcon,
+    rightIcon,
+    onClick,
+    ...passProps
+ }) {
     let Comp = 'button'
+    const props = {
+        onClick,
+        ...passProps
+    }
 
-    const classes = cx('wrapper')
-    return(
-        <Comp classNames={classes}>
-            <span>{children}</span>
+//Remove event listener when btn is disable
+    if(disabled){ 
+        Object.keys(props).forEach(key=>{
+            if(key.startsWith('on')&& typeof props[key]=== 'function'){
+                delete props[key];
+            }
+        })
+    }
+
+    if (to) {
+        props.to = to
+        Comp = 'Link'
+    }
+    else if (href) {
+        props.href = href
+        Comp = 'a'
+    }
+    const classes = cx('wrapper',{
+        [className]: className,
+        primary,
+        outline,
+        text,
+        disabled,
+        rounded,
+        small,
+        large,
+    })
+    return (
+        <Comp className={classes} {...props} >
+            {leftIcon && <span className={cx('icon')}>{leftIcon}</span>}
+            <span className={cx('title')}>{children}</span>
+            {rightIcon && <span className={cx('icon')}>{rightIcon}</span>}
         </Comp>
     )
 }
--- src/component/Layout/component/Header/index.js
+++ src/component/Layout/component/Header/index.js
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import classNames from "classnames/bind"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCircleXmark, faSpinner, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faCircleXmark, faSpinner, faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
 import Tippy from "@tippyjs/react/headless";
 
+import Button from "~/component/Button";
 import { Wrapper as PopperWrapper } from "~/component/Popper";
 import styles from "./Header.module.scss"
 import images from "~/assets/images"
@@ -14,7 +15,7 @@ function Header() {
     const [searchResult, setSearchResult] = useState([])
     useEffect(() => {
         setTimeout(() => {
-            setSearchResult([1, 2])
+            setSearchResult([])
         }, 0)
     }, [])
     return <header className={cx('wrapper')}>
@@ -52,7 +53,10 @@ function Header() {
                 </div>
             </Tippy>
 
-            <div className={cx("action")}>Action</div>
+            <div className={cx("action")}>
+                <Button text>Upload</Button>
+                <Button primary>Log in</Button>
+            </div>
         </div>
     </header>
 }
