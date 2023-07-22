9ddee06c0ebc1430750dddd412fe7d9b5be05910
HoangSon <son.lyhoang2014@gmail.com>
2023-07-22 22:06:20 +0700

    Áp dụng PropTypes vào dự án

--- src/component/image/index.js
+++ src/component/image/index.js
@@ -1,14 +1,29 @@
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import { useState, forwardRef } from 'react';
 import images from '~/assets/images';
-import styles from './image.module.scss'
+import styles from './image.module.scss';
 
-function Images({ src, alt,className, ...props }, ref) {
+function Images({ src, alt, className, ...props }, ref) {
     const [fallback, setFallback] = useState('');
-    const handleError =() =>{
-        setFallback(images.noImage)
-    }
-    return <img className={classNames(styles.wrapper,className)} ref={ref} src={fallback || src} alt={alt} {...props} onError={handleError}/>;
+    const handleError = () => {
+        setFallback(images.noImage);
+    };
+    return (
+        <img
+            className={classNames(styles.wrapper, className)}
+            ref={ref}
+            src={fallback || src}
+            alt={alt}
+            {...props}
+            onError={handleError}
+        />
+    );
 }
-
+Images.propTypes = {
+    src: PropTypes.string,
+    alt: PropTypes.string,
+    className: PropTypes.string,
+    ref: PropTypes.string,
+};
 export default forwardRef(Images);
--- src/layouts/DefaultLayout/index.js
+++ src/layouts/DefaultLayout/index.js
@@ -1,21 +1,22 @@
-import classNames from "classnames/bind"
-import Header from "../component/Header";
-import Sidebar from "./Sidebar";
-import styles from "./DefaultLayout.module.scss"
+import PropTypes from 'prop-types';
+import classNames from 'classnames/bind';
+import Header from '../component/Header';
+import Sidebar from './Sidebar';
+import styles from './DefaultLayout.module.scss';
 
-const cx = classNames.bind(styles)
+const cx = classNames.bind(styles);
 function DefaultLayout({ children }) {
     return (
         <div className={cx('wrapper')}>
             <Header />
-            <div className={cx("container")}>
+            <div className={cx('container')}>
                 <Sidebar />
-                <div className={cx("content")}>
-                    {children}
-                </div>
+                <div className={cx('content')}>{children}</div>
             </div>
         </div>
     );
 }
-
-export default DefaultLayout
+DefaultLayout.propTypes = {
+    children: PropTypes.node.isRequired,
+};
+export default DefaultLayout;
